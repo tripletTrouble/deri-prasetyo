b929b8985b7106667a3b3c7504d97f51eb9067f8
triplettrouble <deri.mbois@gmail.com>
2022-07-11 09:37:58 +0700

    Menambahkan Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './index.html'
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Source Sans Pro', 'system-ui', 'Ubuntu', 'Arial'],
+      display: ['Oswald']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
